7bcad905f84fd14ebe68090ab710c615445a68b7
Muhammad Tsaqif Noor Az Zamil <azzamganteng921@gmail.com>
2026-06-19 17:29:40 +0700

    fix: syntax error in authentication router

--- resources/scripts/routers/AuthenticationRouter.tsx
+++ resources/scripts/routers/AuthenticationRouter.tsx
@@ -37,6 +37,7 @@ export default () => {
                     <NotFound onBack={() => history.push('/auth/login')} />
                 </Route>
             </Switch>
+            </div>
         </div>
     );
 };
